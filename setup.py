f83054f056afc40ceb75db44f4e5b324c58e115c
Pankaj Jarial <pankajjarial.dev@gmail.com>
2026-07-08 10:18:14 +0530

    Update version to pre-release format
    Changed version to 0.1.0rc1 to indicate pre-release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-rest-pgtenants',
-    version='0.1.0',
+    version='0.1.0rc1',  # 🌟 Changed to a pre-release version format
     description='A lightweight, native Django multi-tenant schema isolation framework.',
     author='Pankaj Jarial',
     packages=find_packages(include=['django_rest_pgtenants', 'django_rest_pgtenants.*']),
